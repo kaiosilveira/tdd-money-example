6a263ca27651da0f571e8ce421eb9752b1c703e6
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-28 23:11:20 +0100

    make Franc a subclass of Money
    Tasklist:
    - $5 + 10CHF = $10 if rate is 2:1 🎯
    - $5 * 2 = $10 ✅
    - Make "amount" private ✅
    - Dollar side-effects? ✅
    - Money rounding?
    - equals() ✅
    - Equal null
    - Equal object
    - 5 CHF * 2 = 10 CHF ✅
    - Dollar/Franc duplication
    - Common `.equals` ✅
    - Common `.times`
    - Compare Francs with Dollars

--- src/franc/index.ts
+++ src/franc/index.ts
@@ -1,17 +1,11 @@
-import Money from "../money";
+import Money from '../money';
 
-export default class Franc {
-  private amount: number;
-
+export default class Franc extends Money {
   constructor(amount: number) {
-    this.amount = amount;
+    super(amount);
   }
 
   times(multiplier: number): Franc {
     return new Franc(this.amount * multiplier);
-  }
-
-  equals(object: Object): boolean {
-    return this.amount == (object as Money).amount;
   }
 }
