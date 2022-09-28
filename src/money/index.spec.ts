e47def54aec320ff49d00be9f73aabe6d28894a8
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-28 23:56:55 +0100

    update Franc tests to use factory method from Money
    Tasklist:
    - $5 + 10CHF = $10 if rate is 2:1 🎯
    - $5 * 2 = $10 ✅
    - Make "amount" private ✅
    - Dollar side-effects? ✅
    - Money rounding?
    - equals() ✅
    - Equal null
    - Equal object
    - 5 CHF * 2 = 10 CHF ✅
    - Dollar/Franc duplication
    - Common `.equals` ✅
    - Common `.times` 👈🏼
    - Compare Francs with Dollars ✅
    - Currency?
    - Merge multiplication tests?

--- src/money/index.spec.ts
+++ src/money/index.spec.ts
@@ -3,11 +3,11 @@ import Money, { Dollar, Franc } from '../money';
 describe('Money', () => {
   describe('equality', () => {
     it('should consider an object representing $5 equals to another object representing $5', () => {
-      expect(new Franc(5).equals(new Franc(5))).toBe(true);
-      expect(new Franc(5).equals(new Franc(6))).toBe(false);
+      expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
+      expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
       expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
       expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
-      expect(new Franc(5).equals(new Dollar(5))).toBe(false);
+      expect(Money.franc(5).equals(new Dollar(5))).toBe(false);
     });
   });
 });
@@ -35,17 +35,17 @@ describe('Dollar', () => {
 describe('Franc', () => {
   describe('multiplication', () => {
     it('should multiply $5 by 2 and get $10', () => {
-      const five = new Franc(5);
+      const five = Money.franc(5);
       const product = five.times(2);
-      expect(product.equals(new Franc(10))).toBe(true);
+      expect(product.equals(Money.franc(10))).toBe(true);
     });
 
     it('should preserve the initial dollar amount when multiplying multiple times', () => {
-      const five = new Franc(5);
+      const five = Money.franc(5);
       let product: Franc = five.times(2);
-      expect(product.equals(new Franc(10))).toBe(true);
+      expect(product.equals(Money.franc(10))).toBe(true);
       product = five.times(3);
-      expect(product.equals(new Franc(15))).toBe(true);
+      expect(product.equals(Money.franc(15))).toBe(true);
     });
   });
 });
--- src/money/index.ts
+++ src/money/index.ts
@@ -18,6 +18,10 @@ abstract class Money {
     return new Dollar(amount);
   }
 
+  static franc(amount: number): Money {
+    return new Franc(amount);
+  }
+
   abstract times(multiplier: number): Money;
 }
 
