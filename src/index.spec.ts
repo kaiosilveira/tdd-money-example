bc1534cfc37ef70f69fe96711a4c91abb4f94d2c
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-28 18:10:18 +0100

    add test for multiplying 5 dollars by 2
    Tasklist:
    - $5 + 10CHF = $10 if rate is 2:1 🎯
    - $5 * 2 = $10

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,11 @@
 describe('Multi currency money', () => {
   it.todo('should sum $5 + 10CHF as being $10 if exchange rate is 2:1');
+
+  describe('multiplication', () => {
+    it('should multiply $5 by 2 and get $10', () => {
+      const five = new Dollar(5);
+      five.times(2);
+      expect(five.amount).toEqual(10);
+    });
+  });
 });
