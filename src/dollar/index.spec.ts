e1436757b5585c957342e0eb4ab6c92a83196420
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-28 23:44:45 +0100

    change multiplication test for dollar to use factory method from Money
    Tasklist:
    - $5 + 10CHF = $10 if rate is 2:1 🎯
    - $5 * 2 = $10 ✅
    - Make "amount" private ✅
    - Dollar side-effects? ✅
    - Money rounding?
    - equals() ✅
    - Equal null
    - Equal object
    - 5 CHF * 2 = 10 CHF ✅
    - Dollar/Franc duplication
    - Common `.equals` ✅
    - Common `.times` 👈🏼
    - Compare Francs with Dollars ✅
    - Currency?

--- src/dollar/index.spec.ts
+++ src/dollar/index.spec.ts
@@ -1,11 +1,12 @@
 import Dollar from '.';
+import Money from '../money';
 
 describe('Multi currency money', () => {
   it.todo('should sum $5 + 10CHF as being $10 if exchange rate is 2:1');
 
   describe('multiplication', () => {
     it('should multiply $5 by 2 and get $10', () => {
-      const five = new Dollar(5);
+      const five = Money.dollar(5);
       const product = five.times(2);
       expect(product.equals(new Dollar(10))).toBe(true);
     });
