404e6ecf577c00521e0a221ddb4de6bfe3d2c19a
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-28 20:03:41 +0100

    update test to receive a Dollar as the result of Dollar.times
    Tasklist:
    - $5 + 10CHF = $10 if rate is 2:1 🎯
    - $5 * 2 = $10 ✅
    - Make "amount" private
    - Dollar side-effects? 👈🏼
    - Money rounding?

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -12,10 +12,10 @@ describe('Multi currency money', () => {
 
     it('should preserve the initial dollar amount when multiplying multiple times', () => {
       const five = new Dollar(5);
-      five.times(2);
-      expect(five.amount).toEqual(10);
-      five.times(3);
-      expect(five.amount).toEqual(15);
+      let product: Dollar = five.times(2);
+      expect(product.amount).toEqual(10);
+      product = five.times(3);
+      expect(product.amount).toEqual(15);
     });
   });
 });
