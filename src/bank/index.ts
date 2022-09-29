54e9ce671488811841c8cc197aff528f4036e6ca
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-29 16:44:14 +0100

    add provisional implementation for reducing Money at Bank
    Task list:
    - $5 + 10CHF = $10 if rate is 2:1 🎯
    - $5 + $5 = $10 👈🏼
    - Return Money from $5 + $5
    - Bank.reduce(Money)
    - Reduce Money with conversion
    - Reduce(Bank, String)

--- src/bank/index.ts
+++ src/bank/index.ts
@@ -2,8 +2,9 @@ import Money from '../money';
 import Expression, { Sum } from '../expression';
 
 export default class Bank {
-  reduce(to: Expression, currency: string): Money {
-    const sum: Sum = to as Sum;
-    return sum.reduce(currency);
+  reduce(source: Expression, to: string): Money {
+    if (source instanceof Money) return source as Money;
+    const sum: Sum = source as Sum;
+    return sum.reduce(to);
   }
 }
