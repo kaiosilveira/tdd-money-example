d9b776fd5236e8881194ed8d67fee7110700a701
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-29 17:24:33 +0100

    introduce Bank.addRate
    Task list:
    - $5 + 10CHF = $10 if rate is 2:1 🎯
    - $5 + $5 = $10
    - Return Money from $5 + $5
    - Bank.reduce(Money) ✅
    - Reduce Money with conversion 👈🏼
    - Reduce(Bank, String)

--- src/bank/index.ts
+++ src/bank/index.ts
@@ -2,11 +2,22 @@ import Money from '../money';
 import Expression from '../expression';
 
 export default class Bank {
+  private _rates: Object;
+
+  constructor() {
+    this._rates = {};
+  }
+
   reduce(source: Expression, to: string): Money {
     return source.reduce(this, to);
   }
 
   rate(from: string, to: string): number {
-    return from == 'CHF' && to == 'USD' ? 2 : 1;
+    if (from === to) return 1;
+    return this._rates[`${from}-${to}`];
+  }
+
+  addRate(from: string, to: string, rate: number): void {
+    this._rates[`${from}-${to}`] = rate;
   }
 }
