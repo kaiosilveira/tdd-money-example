2783e4e00923f1d83fe9c221c074c0cf319767fe
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-29 14:44:53 +0100

    introduce test to validate Bank.reduce
    Task list:
    - $5 + 10CHF = $10 if rate is 2:1 🎯
    - $5 + $5 = $10 👈🏼
    - Return Money from $5 + $5

--- src/bank/index.ts
+++ src/bank/index.ts
@@ -1,8 +1,10 @@
-import Expression from '../expression';
 import Money from '../money';
+import Expression, { Sum } from '../expression';
 
 export default class Bank {
   reduce(to: Expression, currency: string): Money {
-    return Money.dollar(10);
+    const sum: Sum = to as Sum;
+    const amount: number = sum.augend.amount + sum.addend.amount;
+    return new Money(amount, currency);
   }
 }
