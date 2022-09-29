9a7580da955aa5d28472ceba0c2117a1e299a49b
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-29 16:51:43 +0100

    introduce test to validate mixed-currency reduction
    Task list:
    - $5 + 10CHF = $10 if rate is 2:1 🎯
    - $5 + $5 = $10
    - Return Money from $5 + $5
    - Bank.reduce(Money) ✅
    - Reduce Money with conversion 👈🏼
    - Reduce(Bank, String)

--- src/bank/index.spec.ts
+++ src/bank/index.spec.ts
@@ -19,6 +19,12 @@ describe('Bank', () => {
         const result: Money = bank.reduce(Money.dollar(1), 'USD');
         expect(result).toEqual(Money.dollar(1));
       });
+
+      it('should reduce mixed Money currencies', () => {
+        const bank: Bank = new Bank();
+        const result: Money = bank.reduce(Money.franc(2), 'USD');
+        expect(result).toEqual(Money.dollar(1));
+      });
     });
   });
 });
