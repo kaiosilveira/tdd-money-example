0d4d03a4f124c2a8f1d64b79bc971a3786213810
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-29 16:57:57 +0100

    add rate method to Bank
    Task list:
    - $5 + 10CHF = $10 if rate is 2:1 🎯
    - $5 + $5 = $10
    - Return Money from $5 + $5
    - Bank.reduce(Money) ✅
    - Reduce Money with conversion 👈🏼
    - Reduce(Bank, String)

--- src/bank/index.ts
+++ src/bank/index.ts
@@ -5,4 +5,8 @@ export default class Bank {
   reduce(source: Expression, to: string): Money {
     return source.reduce(this, to);
   }
+
+  rate(from: string, to: string): number {
+    return from == 'CHF' && to == 'USD' ? 2 : 1;
+  }
 }
