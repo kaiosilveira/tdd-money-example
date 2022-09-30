077e4796bf3e78c48faf737f4b02a49b0d4688bd
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-30 07:27:42 +0100

    add test for Sum.times
    Task list:
    - $5 + 10CHF = $10 if rate is 2:1 🎯 ✅ 🎉 ✨
    - $5 + $5 = $10 ✅
    - Return Money from $5 + $5
    - Bank.reduce(Money) ✅
    - Reduce Money with conversion ✅
    - Reduce(Bank, String) ✅
    - Sum.plus ✅
    - Expression.times 👈🏼

--- src/expression/index.spec.ts
+++ src/expression/index.spec.ts
@@ -16,4 +16,18 @@ describe('Sum', () => {
       expect(result).toEqual(Money.dollar(15));
     });
   });
+
+  describe('times', () => {
+    it('should multiply a sum', () => {
+      const fiveBucks: Expression = Money.dollar(5);
+      const tenFrancs: Expression = Money.franc(10);
+      const bank: Bank = new Bank();
+      bank.addRate('CHF', 'USD', 2);
+
+      const sum = new Sum(fiveBucks, tenFrancs).times(2);
+      const result = sum.reduce(bank, 'USD');
+
+      expect(result).toEqual(Money.dollar(20));
+    });
+  });
 });
